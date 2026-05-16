71d39bfa84f57c2e4c1092ca27b02bdeba54cbc0
baadikhaoula <baadikhaoula38@gmail.com>
2026-05-16 02:27:01 +0200

    mise à jour

--- OneDrive/Desktop/Projet-COO/ang-proj/enset-app-baadi/src/app/products/products.ts
+++ OneDrive/Desktop/Projet-COO/ang-proj/enset-app-baadi/src/app/products/products.ts
@@ -5,5 +5,17 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './products.html',
   styleUrl: './products.css',
+  standalone :true,
 })
-export class Products {}
+export class Products {
+  products : Array<any>;
+  constructor() {
+  }
+  ngOnInit() {
+    this.products = [
+      {id : 1, name : "Computer", price : 2300, selected : true},
+      {id : 2, name : "Printer", price : 1200, selected : false},
+      {id : 3, name : "Smart Phone", price : 1100, selected : true},
+    ]
+  }
+}
